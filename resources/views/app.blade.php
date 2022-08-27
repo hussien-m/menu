2bd8fc53f678cf03617b989b213103805776b738
hussien-m <63204069+hussien-m@users.noreply.github.com>
2022-08-27 12:48:57 +0300

    Lang Add

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -71,7 +71,7 @@
                         <div class="menu-category-page">
                            <div class="place-nav">
                               <div class="place-nav__inner wrapper">
-                                 <a onclick="" href="{{ url()->previous() }}"
+                                 <a href="javascript:void(0);" onclick="javascript:history.go(-1);"
                                     class="back-button focus nuxt-link-active">
                                     <svg width="24"
                                        height="24" viewBox="0 0 24 24" fill="none"
