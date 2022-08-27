d9ea1655d9793198b0cab3cc6bd14f5b6761bec5
hussien-m <63204069+hussien-m@users.noreply.github.com>
2022-08-27 13:27:09 +0300

    Lang Add

--- resources/views/layouts/dashboard/nav.blade.php
+++ resources/views/layouts/dashboard/nav.blade.php
@@ -4,7 +4,7 @@
         <ul class="nav navbar-nav flex-row">
           <li class="nav-item mobile-menu d-md-none mr-auto"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i class="ft-menu font-large-1"></i></a></li>
           <li class="nav-item mr-auto">
-            <a class="navbar-brand" href="index.html">
+            <a class="navbar-brand" href="{{ route("home") }}">
               <img class="brand-logo" alt="modern admin logo" src="{{asset('dash-rtl/app-assets/images/logo/logo.png')}}">
               <h3 class="brand-text">{{ $setting->site_name }}</h3>
             </a>
